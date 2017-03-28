301bec25aae92b4872264cd4346ac336080c11a6
Mariayn <mriayn85@gmail.com>
2017-03-28 22:17:54 +0300

    Add files via upload

---
+++ DebugingExercise.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Substring_broken
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        int jump = int.Parse(Console.ReadLine());
+
+        char[] textChar = text.ToString().ToCharArray();
+        bool hasMatch = false;
+
+        for (int i = 0; i < textChar.Length; i++)
+        {
+            if (textChar[i] == 'p')
+            {
+                hasMatch = true;
+
+                int endIndex = jump;
+
+                if (endIndex > text.Length)
+                {
+                    endIndex = text.Length;
+                }
+
+                string matchedString = text.Substring(i, endIndex+1);
+                Console.WriteLine(matchedString);
+                i += jump;
+            }
+        }
+
+        if (!hasMatch)
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
